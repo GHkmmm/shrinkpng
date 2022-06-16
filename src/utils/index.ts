e0093c41fa49b54e984833222e11432944dbd923
guoming1.huang <guoming.huang@corp.to8to.com>
2022-06-16 11:47:07 +0800

    增加类型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,7 @@ import { IShrinkImageOptions } from "./../types/index";
  * @Author: guoming1.huang
  * @Date: 2022-05-31 14:41:58
  * @LastEditors: guoming1.huang
- * @LastEditTime: 2022-06-16 11:04:51
+ * @LastEditTime: 2022-06-16 11:38:07
  * @FilePath: /shrinkjs/src/utils/index.ts
  * @Description:
  *
@@ -14,7 +14,7 @@ import Shrink from "../shrink";
 export async function shrinkImage(
   file: File,
   { quality }: IShrinkImageOptions = { quality: 80 }
-) {
+): Promise<File> {
   return new Promise((rs, rj) => {
     if (!file) rj(new Error("file can not be empty"));
     new Shrink(file, {
